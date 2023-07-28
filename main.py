53be6bda8b4c391d1e6fa1fc5b3d5aaf387bcd89
Akash Singh <114267538+Akash-Singh04@users.noreply.github.com>
2023-07-28 23:25:05 +0530

    Apparently it had the wrong code this entire time lmao

--- main.py
+++ main.py
@@ -1,56 +1,54 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-class SpeedBot():
-    def __init__(self):
-        #ENTER THE UPLOAD SPEED PROMISED BY YOUR PROVIDER
-        self.Promised_Up=""
-        #ENTER THE DOWNLOAD SPEED PROMISED BY YOUR PROVIDER
-        self.Promised_Down=""
-        #ENTER THE PATH TO YOUR CHROME DRIVER
-        chrome_driver_path= r"C:\Users\ACA$H\Desktop\CONFIDENTIAL\Selenium Driver\chromedriver.exe."
-        service = Service(chrome_driver_path)
-        self.driver = webdriver.Chrome(service=service)
+from pynput.keyboard import Key, Controller
+#Enter Your Login Email For Your Facebook Account
+myemail="example@gmail.com"
+#Enter Your Login Password For Your Facebook Account
+mypass="1234"
+#Enter The Path To Your Driver
+service = Service(r"C:\Users\User\Selenium Driver\chromedriver.exe")
+driver= webdriver.Chrome(service=service)
+driver.get(r"https://tinder.com/app/recs")
+time.sleep(2)
+login_el = driver.find_element(By.XPATH,'//*[@id="q-1470728188"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')
+login_el.click()
+time.sleep(3)
+try:
+    facebook_el= driver.find_element(By.CSS_SELECTOR,'button[aria-label="Log in with Facebook"]')
+    facebook_el.click()
+    
+except:
+    more_el= driver.find_element(By.XPATH,'//*[@id="q1095858032"]/main/div/div[1]/div/div/div[3]/span/button')
+    more_el.click()
+    facebook_el= driver.find_element(By.CSS_SELECTOR,'button[aria-label="Log in with Facebook"]')
+    facebook_el.click()
 
-    def get_internet_speed(self):
-        self.driver.get("https://www.speedtest.net/")
-        start_btn = self.driver.find_element(By.XPATH,'//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
-        start_btn.click()
-        time.sleep(45)
-        download_el=self.driver.find_element(By.XPATH,'//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span')
-        self.actual_download =float(download_el.text)
-        upload_el=self.driver.find_element(By.XPATH,'//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span')
-        self.actual_upload= float(upload_el.text)
-        if self.actual_download<float(self.Promised_Down) or self.actual_upload< float(self.Promised_Up):
-            self.tweet_at_provider()
-
-    def tweet_at_provider(self):
-        self.driver.get("https://twitter.com/home")
-        time.sleep(3)
-        username= self.driver.find_element(By.NAME,"text")
-        #ENTER YOUR USERNAME
-        username.send_keys("")
-        username.send_keys(Keys.ENTER)
-        time.sleep(2)
-        password = self.driver.find_element(By.NAME,"password")
-        #ENTER YOUR PASSWORD
-        password.send_keys("")
-        password.send_keys(Keys.ENTER)
-        time.sleep(5)
-        tweet = f'''My ISP! WHY IS MY INTERNET ONLY {self.actual_download}mbps DOWN/{self.actual_upload}mbps UP
-        WHEN YOU PROMISSED ME {self.Promised_Down}mbps DOWN/{self.Promised_Up}mbps UP!?!??!?!
-        I WILL SUE YOUUUUUU'''
-        tweet_box = self.driver.find_element(By.CLASS_NAME, "public-DraftStyleDefault-block")
-        tweet_box.send_keys(tweet)
-        time.sleep(2)
-        tweet_btn = self.driver.find_element(By.XPATH,'//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]')
-        tweet_btn.click()
-
-speed_bot= SpeedBot()
-speed_bot.get_internet_speed()
-
-
-
-
+time.sleep(2)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+email_el= driver.find_element(By.CSS_SELECTOR,'input[name=email]')
+email_el.send_keys(myemail)
+time.sleep(1)
+pass_el = driver.find_element(By.CSS_SELECTOR,'input[type=password')
+pass_el.send_keys(mypass)
+pass_el.submit()
+time.sleep(5)
+driver.switch_to.window(base_window)
+time.sleep(2)
+allow_el = driver.find_element(By.XPATH,'//*[@id="q1095858032"]/main/div/div/div/div[3]/button[1]')
+allow_el.click()
+time.sleep(2)
+enable_el= driver.find_element(By.XPATH,'//*[@id="q1095858032"]/main/div/div/div/div[3]/button[2]')
+enable_el.click()
+time.sleep(2)
+accept_el=driver.find_element(By.XPATH,'//*[@id="q-1470728188"]/div/div[2]/div/div/div[1]/div[1]/button')
+accept_el.click()
+time.sleep(6)
+for i in range(20):
+    keyboard = Controller()
+    keyboard.press(Key.right)
+    keyboard.release(Key.right)
+    time.sleep(2)
